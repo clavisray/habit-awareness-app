b05d4fab0db4d5509fbcaf7a949667b48a1734d4
clavisray <matbog0@gmail.com>
2026-09-06 09:58:54 +0200

    Initial Commit

---
+++ habit-awareness/habit_awarenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_awarenessApp.swift
+//  habit-awareness
+//
+//  Created by Mateusz Boguszewski on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct habit_awarenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ habit-awarenessTests/habit_awarenessTests.swift
@@ -0,0 +1,19 @@
+//
+//  habit_awarenessTests.swift
+//  habit-awarenessTests
+//
+//  Created by Mateusz Boguszewski on 06/09/2026.
+//
+
+import Testing
+@testable import habit_awareness
+
+struct habit_awarenessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
