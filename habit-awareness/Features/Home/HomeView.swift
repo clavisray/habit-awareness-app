ac7fb17e317b99b6c6cf1cfb2f9eaed2cb2cd2d3
clavisray <matbog0@gmail.com>
2026-09-07 07:40:00 +0200

    Minor changes by changing VStack to LazyVStack for opt. and handling button event adding

--- habit-awareness/Features/Home/HomeView.swift
+++ habit-awareness/Features/Home/HomeView.swift
@@ -9,105 +9,105 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var count1 = 0;
-    @State private var count2 = 0;
-    @State private var count3 = 0;
-    @State private var count4 = 0;
+    @State private var count = 0
     
     @State private var newEvent = ""
     
-    @State private var habits: [Habit] = []
+    @State private var habits: [Habit] = [
+        Habit(title: "Podjadanie", count: 0),
+        Habit(title: "Słodycze", count: 0),
+        Habit(title: "Doomscrolling", count: 0),
+        Habit(title: "Papierosy", count: 0)
+    ]
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            
-            if !newEvent.isEmpty {
-                Image(systemName: "checkmark")
-                    .frame(width: 40, height: 40)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
-            }
-            
-            Text("Today")
-                        
-            HabitCardView(
-                title: "Podjadanie",
-                count: $count1
-            )
-            
-            HabitCardView(
-                title: "Słodycze",
-                count: $count2
-            )
-            
-            HabitCardView(
-                title: "Doomscrolling",
-                count: $count3
-            )
-            
-            HabitCardView(
-                title: "Papierosy",
-                count: $count4
-            )
-            
-            ForEach(habits) {
-                habit in
-                HabitCardView(
-                    title: habit.title,
-                    count: $count1
-                )
-            }
-            
-            HStack{
-                
-                TextField(
-                    "Dodaj nowy nawyk",
-                    text: $newEvent
-                )
+        ScrollView{
+            LazyVStack(alignment: .leading, spacing: 12) {
                 
                 if !newEvent.isEmpty {
-                    Button("+") {
-                        habits.append(
+                    HStack {
+                        Spacer()
+
+                        Button {
+                            habits.append(
                                 Habit(
                                     title: newEvent,
                                     count: 0
                                 )
-                        )
+                            )
+
+                            newEvent = ""
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .frame(width: 40, height: 40)
+                                .background(Color.blue)
+                                .foregroundStyle(.white)
+                                .clipShape(Circle())
+                        }
                     }
-                    .frame(width: 40, height: 40)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
                 }
                 
-            }
-            
-                        
-            /*
-             Button {
+                Text("Today")
                 
-            } label: {
-                HStack {
-                    Image(systemName: "plus")
-                    Text("Dodaj nawyk")
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.secondary)
+                ForEach($habits) {
+                    habit in
+                    HabitCardView(
+                        title: habit.wrappedValue.title,
+                        count: habit.count
+                    )
+                }
+                
+                HStack{
                     
-                    Spacer()
+                    TextField(
+                        "Dodaj nowy nawyk",
+                        text: $newEvent
+                    )
+                    
+                    if !newEvent.isEmpty {
+                        Button("+") {
+                            habits.append(
+                                    Habit(
+                                        title: newEvent,
+                                        count: 0
+                                    )
+                            )
+                            newEvent = ""
+                        }
+                        .frame(width: 40, height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                    }
+                    
                 }
+                
+                            
+                /*
+                 Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Dodaj nawyk")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.secondary)
+                        
+                        Spacer()
+                    }
+                }
+                .frame(maxWidth: .infinity, minHeight: 48)
+                .padding(.horizontal)
+                .background(.gray.opacity(0.12))
+                .foregroundStyle(.primary)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                 */
+                
             }
-            .frame(maxWidth: .infinity, minHeight: 48)
-            .padding(.horizontal)
-            .background(.gray.opacity(0.12))
-            .foregroundStyle(.primary)
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-             */
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
             
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding()
-        
     }
     
 }
